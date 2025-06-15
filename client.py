343c3795dbf761c36299f97f298a44513f6861cd
Al0nL <clashburgerttv@gmail.com>
2025-06-15 10:35:59 -0400

    Update client send server info.py
    updated version of previous commit where data is sent as a list

--- client.py
+++ client.py
@@ -2,10 +2,11 @@ import socket
 pip install psutil
 cpu_usage = psutil.cpu_percent(interval=1)
 memory_usage = psutil.virtual_memory()
+list_to_send = [cpu_usage, memory_usage]
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #the first variable specifies I am using ipv4, the second one specifies tcp
 client.connect(('0.0.0.0', 8080))
 #'0.0.0.0' connects you to the local computer, and you need to replace it with the server IP. 8080 is the port number
-client.send(cpu_usage, memory_usage)
+client.send(list_to_send)
 client.close()
 #closes connection
