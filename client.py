fda0770011637494f1940954b801fcd19279a3df
Al0nL <clashburgerttv@gmail.com>
2025-06-16 07:17:51 -0400

    update to constantly send.py
    this update so that we constantly send the data to the server because we put in an infinite loop to find the CPU and ram utilisation rate and then we connect and send it to the server all under an infinite loop.

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 import socket
 pip install psutil
-cpu_usage = psutil.cpu_percent(interval=1)
-memory_usage = psutil.virtual_memory()
-list_to_send = [cpu_usage, memory_usage]
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+while true:
+  cpu_usage = psutil.cpu_percent(interval=1)
+  memory_usage = psutil.virtual_memory()
+  list_to_send = [cpu_usage, memory_usage]
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #the first variable specifies I am using ipv4, the second one specifies tcp
-client.connect(('0.0.0.0', 8080))
+  client.connect(('0.0.0.0', 8080))
 #'0.0.0.0' connects you to the local computer, and you need to replace it with the server IP. 8080 is the port number
-client.send(list_to_send)
-client.close()
+  client.send(list_to_send)
+  client.close()
 #closes connection
